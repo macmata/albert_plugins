49782770aa8526054f8dcd7d4e3f45800d5a99a1
Alexandre Leblanc <info@alexandre-leblanc.com>
2018-01-06 13:38:11 -0500

    first commit
    I needed to be able to access my notes without having to open
    a terminal and grep for a specfic expression.
    
    Selected note is copied in buffer.

---
+++ Egrep.py
@@ -0,0 +1,44 @@
+from albertv0 import *
+import subprocess
+import os
+
+# see https://albertlauncher.github.io/docs/extensions/python/
+
+__iid__ = "PythonInterface/v0.1"
+__prettyname__ = "Egrep"
+__version__ = "0.0.1"
+__trigger__ = "egrep"
+__author__ = "Alexandre Leblanc"
+
+HOME_DIR = os.environ["HOME"]
+NOTES_DIR = os.environ.get("NOTES_DIR", os.path.join(HOME_DIR, "notes"))
+
+
+def handleQuery(query):
+    if query.isTriggered:
+        return agregate(query)
+
+
+def run(query):
+    base_command = "egrep -r"
+    process = subprocess.Popen((base_command+" "+query.string+" "+NOTES_DIR).split(), stdout=subprocess.PIPE)
+    comm_tuples = process.communicate()
+    return comm_tuples[0].splitlines()
+
+
+def agregate(query):
+    results = run(query)
+    items = []
+    for line in results:
+        r = line[-(len(line) - len(NOTES_DIR)-1):]
+        items.append(
+            Item(
+                id=__prettyname__,
+                text="egrep {0}".format(r),
+                actions=[
+                    ClipAction("Copied to buffer", r)
+                ]
+            )
+        )
+    return items
+
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pkill albert
+cp Egrep.py ~/.local/share/albert/org.albert.extension.python/modules/Egrep.py
